0bbd46ebaab36608cd4a03fc0ef98f792de8d03a
bobbyboy2007 <103608477+bobbyboy2007@users.noreply.github.com>
2022-09-19 12:47:47 +1200

    Add files via upload

---
+++ lesson 2/lesson 2.py
@@ -0,0 +1,16 @@
+ # A S Samdhu
+ # Basic claculator
+
+ # On-screen instruction with options
+ print("Select an operation to perform:")
+ print("1.  ADD")
+ print("2.  SUBTRACT")
+ print("3.  MULTIPLY")
+ print("4.  Divide")
+
+ # set the inputfunction
+ Operation = input()
+
+ If operation == "1"
+     # code for add
+      
